1b4f54a0e7db31e0ad26f5b8494a634fa98caced
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-03-12 18:04:49 +0100

    Another python lesson, now I'll continue searching threejs slider inspirations

--- Lesson2.py
+++ Lesson2.py
@@ -1,3 +1,9 @@
 number = "20"
 age = int(number)
-print(isinstance(age, int))
+# print(isinstance(age, int))
+
+age = 19
+
+check = True if age > 18 else False
+
+print("""        Something is happen""".strip())
